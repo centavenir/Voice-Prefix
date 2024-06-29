a63d21146566c7d8084ac422e97e08edcedcfd5c
Dexterity <diviniterishere@yahoo.com>
2024-06-29 14:23:04 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    clients: {
+        token: '', // token
+        prefix: '', // prefix
+        activity: '...', // activité du bot
+        embedColor: '#ff0000', // couleur de l'embed
+        name: 'centavenir', // texte de l'embed
+        logo: 'https://cdn.discordapp.com/avatars/1212806739666276546/71883e70768de2e50dbb6ca386e15ee0.png?size=1024', // image de l'embed
+        buyer: '' // ton id
+    }
+}
